2c4e211b4c295502483269b06707f1adfcc1c69e
Tushar1401 <tushararora1401@gmail.com>
2022-08-27 14:53:42 +0530

    all files added

---
+++ src/pages/Landing.js
@@ -0,0 +1,30 @@
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Keeping track of job applications while they are in progress is a tedious process. The Job Keeper App helps you to do that. It has a dashboard where you can filter your pending and applied applications by type, name, or how latest the application is.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
