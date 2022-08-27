0da9e64f59f9e7fe30593d3f862a78e8ebee45ea
Tushar1401 <tushararora1401@gmail.com>
2022-08-27 17:53:19 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { render } from 'react-dom';
+// import { createRoot } from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 
 // const container = document.getElementById('root');
 // const root = createRoot(container);
